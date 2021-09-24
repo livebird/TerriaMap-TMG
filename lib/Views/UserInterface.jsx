4ba8e8241f5418a4a3f9e2ac33449a3c9ef00bc2
Vishal Parekh <vishalparekh130@gmail.com>
2021-09-24 11:18:53 +0530

    - Add sendEventToDevice Function for an event.

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -29,6 +29,237 @@ import "./global.scss";
 // }
 
 export default function UserInterface(props) {
+  const aboutEnabled = props.terria.configParameters.aboutEnabled;
+  const relatedMapsEnabled = props.terria.configParameters.relatedMapsEnabled;
+
+  function sendEventToDevice(event) {
+    var ifrm = document.createElement("IFRAME");
+    ifrm.setAttribute("src", "js-frame:" + event);
+    document.documentElement.appendChild(ifrm);
+    ifrm.parentNode.removeChild(ifrm);
+    ifrm = null;
+  }
+
+  const eventArray = {
+    onClick: function(next) {
+      if (
+        props.terria.mainViewer._lastViewer &&
+        props.terria.mainViewer._lastViewer.scene &&
+        props.terria.mainViewer._lastViewer.scene.map &&
+        props.terria.mainViewer._lastViewer.scene.map._events &&
+        props.terria.mainViewer._lastViewer.scene.map._events.click
+      ) {
+        var newPickLocation =
+          props.terria.mainViewer._lastViewer.scene.map._events.click[0].fn;
+        props.terria.mainViewer._lastViewer.scene.map._events.click[0].fn = function(
+          e
+        ) {
+          console.log("new Pratik");
+          newPickLocation(e);
+          window.currentLatLong = {
+            latitude: props.terria.leaflet.mouseCoords.latitude,
+            longitude: props.terria.leaflet.mouseCoords.longitude
+          };
+          console.log(window.currentLatLong);
+          sendEventToDevice("onClick");
+        };
+      } else if (next) {
+        next(next);
+      }
+    },
+    move: function(next) {
+      if (
+        props.terria.mainViewer._lastViewer &&
+        props.terria.mainViewer._lastViewer.scene &&
+        props.terria.mainViewer._lastViewer.scene.map &&
+        props.terria.mainViewer._lastViewer.scene.map._events &&
+        props.terria.mainViewer._lastViewer.scene.map._events.move
+      ) {
+        var newPickLocation =
+          props.terria.mainViewer._lastViewer.scene.map._events.move[0].fn;
+        props.terria.mainViewer._lastViewer.scene.map._events.move[0].fn = function(
+          e
+        ) {
+          console.log("move Pratik");
+          newPickLocation(e);
+          window.currentLatLong = {
+            latitude: props.terria.leaflet.mouseCoords.latitude,
+            longitude: props.terria.leaflet.mouseCoords.longitude
+          };
+          console.log(window.currentLatLong);
+          sendEventToDevice("move");
+        };
+      } else if (next) {
+        next(next);
+      }
+    },
+    zoomend: function(next) {
+      if (
+        props.terria.mainViewer._lastViewer &&
+        props.terria.mainViewer._lastViewer.scene &&
+        props.terria.mainViewer._lastViewer.scene.map &&
+        props.terria.mainViewer._lastViewer.scene.map._events &&
+        props.terria.mainViewer._lastViewer.scene.map._events.zoomend
+      ) {
+        var newPickLocation =
+          props.terria.mainViewer._lastViewer.scene.map._events.zoomend[0].fn;
+        props.terria.mainViewer._lastViewer.scene.map._events.zoomend[0].fn = function(
+          e
+        ) {
+          console.log("zoomend Pratik");
+          newPickLocation(e);
+          window.currentLatLong = {
+            latitude: props.terria.leaflet.mouseCoords.latitude,
+            longitude: props.terria.leaflet.mouseCoords.longitude
+          };
+          console.log(window.currentLatLong);
+          sendEventToDevice("zoomend");
+        };
+      } else if (next) {
+        next(next);
+      }
+    }
+  };
+
+  function triggerAllEvents() {
+    for (const key in eventArray) {
+      if (Object.hasOwnProperty.call(eventArray, key)) {
+        const element = eventArray[key];
+        const newFunc = function(next) {
+          setTimeout(function() {
+            element(next);
+          }, 1000);
+        };
+        newFunc(newFunc);
+      }
+    }
+  }
+
+  triggerAllEvents();
+
+  props.terria.locationService = function(zoomToLocation) {
+    /*
+    My location code
+     */
+    window.zoomToMyLocation = function(latitude, longitude, error = undefined) {
+      if (latitude !== undefined && longitude !== undefined) {
+        zoomToLocation({
+          coords: {
+            latitude: latitude,
+            longitude: longitude
+          }
+        });
+      } else if (error !== undefined) {
+        //Show alert
+      }
+    };
+    $.ajax({
+      url: "./getLocation",
+      method: "POST",
+      success: function(result) {
+        if (result.isSuccess !== undefined && result.isSuccess === true) {
+          window.zoomToMyLocation(result.latitude, result.longitude);
+        } else if (result.message !== undefined) {
+          //Show alert
+        }
+      }
+    });
+  };
+
+  /*
+    Go to coordinate code
+     */
+  props.terria.gotoCoordinate = function(gotoCoordinate) {
+    window.gotoCoordinate = function(latitude, longitude) {
+      gotoCoordinate(latitude, longitude);
+    };
+  };
+
+  props.terria.getCenterLatLong = function(getCenterLatLong) {
+    window.getCenterLatLong = function() {
+      return getCenterLatLong();
+    };
+  };
+
+  /**
+   *
+   * @param {Object} baseMap : viewState.terria.baseMaps[index]
+   * Change index to update map type, refer baseMaps array for more details
+   * @use : selectBaseMap(viewState.terria.baseMaps[0])
+   */
+  const mapTypes = {
+    "Bing Maps Aerial with Labels": "basemap-bing-aerial-with-labels",
+    "Bing Maps Aerial": "basemap-bing-aerial",
+    "Bing Maps Roads": "basemap-bing-roads",
+    "Natural Earth 2": "basemap-natural-earth-II",
+    "NASA Black Marble": "basemap-black-marble",
+    "Postitron(Light)": "basemap-positron",
+    "Dark Matter": "basemap-darkmatter",
+    "Here Terrain": "basemap-here-terrain",
+    "Here Satellite": "basemap-here-satellite",
+    "Thunderforest Outdoors": "basemap-thunderforest-outdoors",
+    "Thunderforest Transport Dark": "basemap-thunderforest-transport-dark",
+    "ESRI World Imagenary Basemap": "basemap-esri-world-imagery-basemap",
+    "ESRI Shaded Relief Basemap": "basemap-esri-shaded-relief",
+    "ESRI World Street Map Basemap": "basemap-esri-world-street-map",
+    "ESRI NatGeo World Map Basemap": "basemap-esri-natGeo-world-map"
+  };
+  /**
+   *
+   * @param {} mapJson
+   */
+  function getBaseMap(maptype) {
+    const basemaps = props.viewState.terria.baseMaps;
+    for (let i = 0; i < basemaps.length; i++) {
+      if (basemaps[i].mappable.uniqueId == mapTypes[maptype]) {
+        return basemaps[i];
+      }
+    }
+  }
+  window.selectBaseMap = function(mapJson) {
+    const baseMap = getBaseMap(mapJson.maptype);
+    props.terria.mainViewer.setBaseMap(baseMap.mappable);
+
+    if (baseMap.mappable) {
+      const baseMapId = baseMap.mappable.uniqueId;
+      if (baseMapId) {
+        props.terria.setLocalProperty("basemap", baseMapId);
+      }
+    }
+  };
+
+  window.changeTimeline = function(flag) {
+    runInAction(() => {
+      if (flag) {
+        props.terria.timelineStack.defaultTimeVarying = new DefaultTimelineModel();
+      } else {
+        props.terria.timelineStack.defaultTimeVarying = undefined;
+      }
+    });
+  };
+  window.mapQuality = function(val) {
+    runInAction(() => {
+      props.terria.baseMaximumScreenSpaceError = val || 0;
+    });
+  };
+  window.selectViewer = function(viewer) {
+    runInAction(() => {
+      const mainViewer = props.terria.mainViewer;
+      if (viewer === "3d" || viewer === "3dsmooth") {
+        mainViewer.viewerMode = "cesium";
+        mainViewer.viewerOptions.useTerrain = viewer === "3d";
+      } else if (viewer === "2d") {
+        mainViewer.viewerMode = "leaflet";
+      } else {
+        console.error(
+          `Trying to select ViewerMode ${viewer} that doesn't exist`
+        );
+      }
+      // We store the user's chosen viewer mode for future use.
+      props.terria.setLocalProperty("viewermode", viewer);
+      props.terria.currentViewer.notifyRepaintRequired();
+    });
+  };
   return (
     <StandardUserInterface {...props} version={version}>
       <MenuLeft>
